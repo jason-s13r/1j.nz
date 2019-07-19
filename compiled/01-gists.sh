eece38c5b83a44edcfaf93357ec476010d0020bf
Jason Schwarzenberger <jason@js.org.nz>
2019-07-19 04:15:39 +0000

    escape

--- compiled/01-gists.sh
+++ compiled/01-gists.sh
@@ -1,4 +1,4 @@
-echo "	<section class="gists">"
+echo "	<section class=\"gists\">"
 echo "		<h3>Gists</h3>"
 curl --silent -s "https://api.github.com/users/master5o1/gists" \
  | jq '.[0:10] | map("<article class=\"gist\"><a href=\"\(.html_url)\">\(.files | keys | .[0])</a><time datetime=\"\(.created_at)\">\(.created_at)</time><p class=\"caption\">\(.description)</p></article>") | add' \
