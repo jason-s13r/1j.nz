dd38cf05a792947ccff4e4d5cfcca14a1e2277cb
Jason <jason@1j.nz>
2020-06-03 04:44:59 +0000

    update

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -8,6 +8,10 @@ git pull
 
 source ./env/bin/activate
 python $dir/scripts/gists.py
+
+rm $dir/public
+rm /var/www/1j.nz/public
+
 /home/linuxbrew/.linuxbrew/bin/hugo
 /home/linuxbrew/.linuxbrew/bin/hugo -b https://1j.nz/ -d /var/www/1j.nz/public
 
