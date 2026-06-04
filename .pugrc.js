f1aac2fe7b715505f3ddbe4de333bd4afb236ccc
Jason Schwarzenberger <jason@js.org.nz>
2026-06-04 13:22:09 +1200

    config backed _main.pug.

--- .pugrc.js
+++ .pugrc.js
@@ -1,8 +1,10 @@
 const postUtils = require("./src/core/pug/posts");
 const pagingUtils = require("./src/core/pug/paging");
+const site = require('./site.json');
 
 module.exports = {
   locals: {
+    site,
     pageNum: 1,
     pageSize: 10,
     pagedList: pagingUtils.pagedList,
