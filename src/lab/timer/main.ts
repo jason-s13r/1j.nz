954cfd73a8ff8b927acbd7f4aaeaa541b65327e0
Jason Schwarzenberger <jason@js.org.nz>
2026-07-24 00:34:43 +1200

    fix: update Timer import path after component reorganization

--- src/lab/timer/main.ts
+++ src/lab/timer/main.ts
@@ -1,5 +1,5 @@
 import r2wc from "@r2wc/react-to-web-component";
-import Timer from "../../components/timer/Timer";
+import Timer from "./components/Timer";
 
 const WebTimer = r2wc(Timer, {
   props: {
